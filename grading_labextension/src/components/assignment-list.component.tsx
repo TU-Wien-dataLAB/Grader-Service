61b4c05a40d03c680dff5e80963e07a7e26d5d50
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-04-06 13:19:19 +0000

    Styling of assignment list

--- grading_labextension/src/components/assignment-list.component.tsx
+++ grading_labextension/src/components/assignment-list.component.tsx
@@ -4,6 +4,9 @@ import { Collapse } from '@jupyterlab/ui-components'
 import { Assignment } from '../model/assignment';
 import { AssignmentComponent } from './assignment.component';
 
+import { Icon } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+
 export interface AssignmentListProps {
   lectureId: number; // assignment id
   title: string; // course title
@@ -38,13 +41,18 @@ export class AssignmentsComponent extends React.Component<AssignmentListProps> {
 
   public render() {
     return <div className="AssignmentsComponent">
-      <div onClick={this.toggleOpen} className="collapse-header">{this.title}</div>
-      <Collapse isOpen={this.state.isOpen} className="collapse-body">
+      <div onClick={this.toggleOpen} className="collapse-header">
+        <Icon icon={IconNames.LEARNING} className="flavor-icon"></Icon>
+        {this.title} 
+        <Icon iconSize={Icon.SIZE_LARGE} icon={IconNames.CHEVRON_DOWN} className={`collapse-icon ${this.state.isOpen ? "collapse-icon-open" : ""}`}></Icon> 
+        </div>
+      <Collapse isOpen={this.state.isOpen} className="collapse-body" transitionDuration={300} keepChildrenMounted={true}>
         <ul>
           {this.state.assignments.map((el, index) =>
-            <AssignmentComponent index={index} assignment={el} />
+            <AssignmentComponent index={index} lectureName={this.title} assignment={el} />
             )}
         </ul>
+        
       </Collapse>
     </div>;
   }
--- grading_labextension/src/components/assignment.component.tsx
+++ grading_labextension/src/components/assignment.component.tsx
@@ -1,27 +1,78 @@
 import * as React from 'react';
 import { Assignment } from '../model/assignment';
 
+import { Icon, Collapse } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+
+import { GlobalObjects } from '../index';
+import { showErrorMessage } from '@jupyterlab/apputils'
+
+// import { DockLayout, Widget } from '@lumino/widgets';
+
 export interface AssignmentProps {
   index: number;
+  lectureName: string,
   assignment: Assignment;
 }
 
 export class AssignmentComponent extends React.Component<AssignmentProps> {
   public assignment: Assignment;
+  public lectureName: string;
   public index: number;
+  public state = {
+    isOpen: true,
+  };
 
   constructor(props: AssignmentProps) {
     super(props);
     this.assignment = props.assignment;
     this.index = props.index;
+    this.lectureName = props.lectureName;
+  }
+
+  private toggleOpen = () => {
+    console.log("toggle assignment header")
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  private openFile(path: string) {
+    console.log("Opening file: " + path)
+    GlobalObjects.commands.execute('docmanager:open', {
+      path: path,
+      options: {
+        mode: 'tab-after' // tab-after tab-before split-bottom split-right split-left split-top
+      }
+    }).catch(error => {
+      showErrorMessage("Error Opening File", error)
+    })
   }
 
   public render() {
     return <li key={this.index}>
-      <div>
-        <p>{this.assignment.name}</p>
-        {this.assignment.exercises.map((ex, i) => <div>Exercise: {ex.name}</div>)}
-        {this.assignment.files.map((file, i) => <div>File: {file.name}</div>)}
+      <div className="assignment">
+        <div onClick={this.toggleOpen} className="assignment-header">
+          <Icon icon={IconNames.CHEVRON_RIGHT} iconSize={12}
+            className={`collapse-icon-small ${this.state.isOpen ? "collapse-icon-small-open" : ""}`}></Icon>
+          <Icon icon={IconNames.INBOX} iconSize={12} className="flavor-icon"></Icon>
+          {this.assignment.name}
+        </div>
+        <Collapse isOpen={this.state.isOpen}>
+          <div className="assignment-content">
+            {this.assignment.exercises.map((ex, i) =>
+              <div className="list-element" onClick={() => this.openFile(`${this.lectureName}/${this.assignment.name}/${ex.name}`)}>
+                <Icon icon={IconNames.EDIT} iconSize={12} className="flavor-icon"></Icon>
+                {ex.name}
+              </div>
+            )}
+
+            {this.assignment.files.map((file, i) =>
+              <div className="list-element" onClick={() => this.openFile(`${this.lectureName}/${this.assignment.name}/${file.name}`)}>
+                <Icon icon={IconNames.DOCUMENT} iconSize={12} className="flavor-icon"></Icon>
+                {file.name}
+              </div>
+            )}
+          </div>
+        </Collapse>
       </div>
     </li>
   }
