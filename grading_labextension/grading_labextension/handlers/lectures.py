b6a7e1e6f1968221147e611cbc19b49d0034dd43
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-04-15 09:17:58 +0000

    Adds skeletons of handlers

--- grading_labextension/grading_labextension/handlers/lectures.py
+++ grading_labextension/grading_labextension/handlers/lectures.py
@@ -1,4 +1,4 @@
-from grading_labextension.handlers.registry import register_handler
+from common.registry import register_handler
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
@@ -12,14 +12,14 @@ class LectureBaseHandler(APIHandler):
     pass
 
 
-@register_handler(path=r"(\/lectures)\/(?P<lecture_id>\d*)\/?")
+@register_handler(path=r"\/lectures\/(?P<lecture_id>\d*)\/?")
 class LectureObjectHandler(APIHandler):
-  def put(self, lecture_id):
+  def put(self, lecture_id: int):
     pass 
   
-  def get(self, lecture_id):
+  def get(self, lecture_id: int):
     pass
   
-  def delete(self, lecture_id):
+  def delete(self, lecture_id: int):
     pass
 
