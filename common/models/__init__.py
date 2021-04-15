9b90d3186f82210240efe4ce3870d45cb001c41c
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-04-15 18:23:39 +0000

    Fixes api schema and fixes model generation

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -7,7 +7,6 @@ from common.models.assignment import Assignment
 from common.models.assignment_file import AssignmentFile
 from common.models.error_message import ErrorMessage
 from common.models.exercise import Exercise
-from common.models.feedback import Feedback
 from common.models.grading_result import GradingResult
 from common.models.inline_response200 import InlineResponse200
 from common.models.inline_response200_data import InlineResponse200Data
@@ -15,5 +14,8 @@ from common.models.inline_response200_submissions import InlineResponse200Submis
 from common.models.lecture import Lecture
 from common.models.manual_grading_content import ManualGradingContent
 from common.models.manual_grading_content_feedback_cells import ManualGradingContentFeedbackCells
+from common.models.rich_assignment import RichAssignment
+from common.models.rich_feedback import RichFeedback
+from common.models.rich_submission import RichSubmission
 from common.models.submission import Submission
 from common.models.user import User
