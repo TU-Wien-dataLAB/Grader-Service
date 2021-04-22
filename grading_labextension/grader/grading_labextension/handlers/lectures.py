b89a40d36b9d52afb8918afc6e61ed409d89ba30
fjaeger <florian.jaeger@tuwien.ac.at>
2021-04-22 12:04:36 +0200

    added get/post requests to lectures and assignments

--- grading_labextension/grader/grading_labextension/handlers/lectures.py
+++ grading_labextension/grader/grading_labextension/handlers/lectures.py
@@ -2,24 +2,28 @@ from grader.common.registry import register_handler
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
-from grader.common.services.request import request
+from grader.common.services.request import RequestService
 
 @register_handler(path=r"\/lectures\/?")
 class LectureBaseHandler(APIHandler):
+  requestservice = RequestService()
   def get(self):
-    pass    
+    self.write(self.requestservice.request(method='GET',endpoint=self.request.path,body=''))
 
-  def post(self):
-    pass
+  def post(self, lecture_id: int):
+    self.write(self.requestservice.request(method='POST',endpoint=self.request.path,body=lecture_id))
+
 
 
 @register_handler(path=r"\/lectures\/(?P<lecture_id>\d*)\/?")
 class LectureObjectHandler(APIHandler):
+  requestservice = RequestService()
   def put(self, lecture_id: int):
     pass 
   
   def get(self, lecture_id: int):
-    pass
+    self.write(self.requestservice.request(method='GET',endpoint=self.request.path+'/'+lecture_id,body=''))
+
   
   def delete(self, lecture_id: int):
     pass
