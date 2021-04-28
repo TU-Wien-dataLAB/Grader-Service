54e9b7e4a92aee584c1932ea94bd93d99d41f976
fjaeger <florian.jaeger@tuwien.ac.at>
2021-04-28 13:44:36 +0200

    more merge

--- service/grader/service/persistence/lectures.py
+++ service/grader/service/persistence/lectures.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from grader.common.models.user import User
-<<<<<<< HEAD
 from grader.common.models.lecture import Lecture
-=======
 from grader.service.persistence.database import DataBaseManager
->>>>>>> 19c9b3d65a4fc595d85197ca1d798ef5676cdfbd
 import json
 
 def get_lectures(user: User):
@@ -18,7 +15,7 @@ def get_lectures(user: User):
     return res
 
 def get_lecture(user: User, lectid: int):
-    engine = create_engine('sqlite:///grader.db', echo=True)
+    engine = create_engine(DataBaseManager.get_database_url(), echo=True)
     session = Session(bind=engine)
     select = "SELECT lecture.* FROM takepart INNER JOIN lecture ON lecture.id=takepart.lectid WHERE userid=%i AND lecture.id=%i" % (user.id,lectid)
     res = session.execute(select)
@@ -27,10 +24,11 @@ def get_lecture(user: User, lectid: int):
     return res
 
 def create_lecture(user: User, lecture: Lecture):
-    engine = create_engine('sqlite:///grader.db', echo=True)
+    engine = create_engine(DataBaseManager.get_database_url(), echo=True)
     session = Session(bind=engine)
     select = 'INSERT INTO "lecture" ("name","semester","code") VALUES ("%s","%s","%s")' % (lecture.name,lecture.semester,lecture.code) 
     session.execute(select)
+
     select = 'INSERT INTO "takepart" ("userid","lectid","role") VALUES (%i,%i,"%s")' % (user.id, lecture.id,"admin")
     session.execute(select)
 
@@ -38,5 +36,5 @@ def create_lecture(user: User, lecture: Lecture):
 
 
 #print(get_lecture(User(1,"user1"),2))
-create_lecture(User(1,"user1"),Lecture(name="lecture1",code="dsdsdd",complete=False,semester="WS22"))
+#create_lecture(User(1,"user1"),Lecture(name="lecture1",code="dsdsdd",complete=False,semester="WS22"))
 print(get_lectures(User(1,"user1")))
