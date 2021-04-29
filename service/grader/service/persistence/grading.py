618aa1cb24057d9eccf7430dc3b84cf760607753
fjaeger <florian.jaeger@tuwien.ac.at>
2021-04-29 14:40:19 +0200

    added submission and grading requests

---
+++ service/grader/service/persistence/grading.py
@@ -0,0 +1,15 @@
+from grader.service.persistence.database import DataBaseManager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from grader.common.models.user import User
+import datetime
+import json
+
+def get_score(subid: int):
+    engine = create_engine(DataBaseManager.get_database_url(), echo=True)
+    session = Session(bind=engine)
+    select = 'SELECT score FROM feedback WHERE subid=%i' % (subid)
+    res = session.execute(select)
+    res = json.dumps([dict(x) for x in res])
+    session.commit()
+    return res
--- service/grader/service/persistence/submissions.py
+++ service/grader/service/persistence/submissions.py
@@ -5,14 +5,18 @@ from grader.common.models.user import User
 import datetime
 import json
 
-def get_submissions(assignid: int, user: User):
+def get_submissions(assignid: int, user: User, latest: bool):
     engine = create_engine(DataBaseManager.get_database_url(), echo=True)
     session = Session(bind=engine)
-    select = 'SELECT * FROM "submission" WHERE assignid=%i AND username="%s" ORDER BY date DESC' % (assignid, user.name)
+    if latest:
+        select = 'SELECT * FROM "submission" WHERE assignid=%i AND username="%s" ORDER BY date DESC LIMIT 1' % (assignid, user.name)
+    else:
+        select = 'SELECT * FROM "submission" WHERE assignid=%i AND username="%s" ORDER BY date DESC' % (assignid, user.name)
     res = session.execute(select)
     res = json.dumps([dict(x) for x in res])
     session.commit()
     return res
+
 
 def submit_assignment(assignid: int, user: User):
     engine = create_engine(DataBaseManager.get_database_url(), echo=True)
