5a3eb91a3c96eb8b8aa35252f434811995d42e33
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-05-05 11:03:02 +0000

    Adds relashionships to ORMs

--- service/grader/service/orm.py
+++ service/grader/service/orm.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum, Boolean
+from sqlalchemy.orm import relationship
+# import to get classes before definition
+from .orm import Feedback, Submission, Assignment, File, Lecture, Role, User
 
 
 Base = declarative_base()
@@ -8,10 +11,11 @@ class User(Base):
     __tablename__ = 'user'
     name =  Column(String(255), primary_key=True)
 
+    lectures = relationship(Lecture, secondary=Role, back_populates="users")
 
 class Feedback(Base):
   __tablename__ = "feedback"
-  Column('id', Integer, primary_key=True, autoincrement=True)
+  Column('id', Integer, ForeignKey('submission.id'), primary_key=True)
   Column('score', Integer, nullable=False)
 
 class Submission(Base):
@@ -20,7 +24,9 @@ class Submission(Base):
   Column('date', DateTime, nullable=False)
   Column('status', Enum('submitting', 'not_graded', 'automatically_graded', 'manually_graded'), default='submitting', nullable=False)
   Column('assignid', Integer, ForeignKey('assignment.id'))
-  Column('username', Integer, ForeignKey('user.name'))
+
+  assignment = relationship(Assignment, back_populates="submissions")
+  feedback = relationship(Feedback)
 
 class Assignment(Base):
   __tablename__ = "assignment"
@@ -32,6 +38,10 @@ class Assignment(Base):
   Column('points', Integer, nullable=False)
   Column('status', Enum('created', 'released', 'fetching', 'fetched', 'complete'), default='created')
 
+  lecture = relationship(Lecture, back_populates="assignments")
+  files = relationship(File, back_populates="assignment")
+  submissions = relationship(Submission, back_populates="assignment")
+
 class File(Base):
   __tablename__ = "file"
   Column('id', Integer, primary_key=True, autoincrement=True)
@@ -40,6 +50,8 @@ class File(Base):
   Column('path', String(255), nullable=False)
   Column('exercise', Boolean, nullable=False)          
   Column('points', Integer)
+
+  assignment = relationship(Assignment, back_populates="files")
 
 class Role(Base):
   __tablename__ = "takepart"
@@ -54,3 +66,6 @@ class Lecture(Base):
   Column('semester',String(255), nullable=False)
   Column('code', String(255), nullable=False)
   Column('complete',Boolean(), default=False)
+
+  assignments = relationship(Assignment, back_populates="lecture")
+  users = relationship(User, secondary=Role, back_populates="lectures")
