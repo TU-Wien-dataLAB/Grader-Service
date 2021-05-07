62bc3b0046f7fd2a6eca7e36e063f63e25e207cf
fjaeger <florian.jaeger@tuwien.ac.at>
2021-05-07 13:50:19 +0200

    added test for user and submission

--- service/tests/persistence/test_user.py
+++ service/tests/persistence/test_user.py
@@ -1,3 +1,14 @@
+from grader.common.models.user import User
+import pytest
+from unittest.mock import MagicMock
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import false
+from .db_util import *
+from grader.service.persistence.database import DataBaseManager
+from grader.service.persistence import user
 
-def test_create_user():
-  pass
+def test_create_user(full_db):
+  setup_db_manager_mock(full_db)
+  user.create_user(User("test"))
+  assert user.user_exists(User("test"))
