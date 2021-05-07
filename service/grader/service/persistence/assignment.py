3cdc0b38da9aae01b1c1f5b8c6b9e87b127ffbcd
fjaeger <florian.jaeger@tuwien.ac.at>
2021-05-07 10:01:34 +0200

    changed assignments and submission

--- service/grader/service/persistence/assignment.py
+++ service/grader/service/persistence/assignment.py
@@ -5,63 +5,61 @@ from grader.service.persistence.database import DataBaseManager
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from grader.common.models.user import User
+from grader.service import orm
 from grader.common.models.assignment import Assignment
 import json
 
 def get_assignments(lectid: int) -> List[Assignment]:
     session = DataBaseManager.instance().create_session()
 
-    select = text("SELECT * FROM assignment WHERE lectid=:id ORDER BY duedate ASC")
-    select = select.bindparams(id=lectid)
-    res = session.execute(select)
-    res = [dict(ix) for ix in res]
-    res = [Assignment.from_dict({"id": d["id"], "name": d["name"], "due_date": d["duedate"], "status": d["status"]} for d in res)]
+    select = session.query(orm.Assignment).filter(orm.Assignment.lectid == lectid).all()
     session.commit()
-    return res
+    return select
 
 #used for fechting specific assignment
 def get_assignment(lectid: int, assignid: int):
     session = DataBaseManager.instance().create_session()
 
-    select = text("SELECT * FROM assignment WHERE lectid=:lectid AND id=:id")
-    select = select.bindparams(lectid=lectid,id=assignid)
-    res = session.execute(select)
-    res = [dict(ix) for ix in res]
+    select = session.query(orm.Assignment).filter(orm.Assignment.lectid == lectid, orm.Assignment.id == assignid).one()
     session.commit()
-    return res
+    return select
 
 def create_assignment(assignment: Assignment, lectid: int):
     session = DataBaseManager.instance().create_session()
 
-    insert = text("INSERT INTO 'assignment' ('name','lectid','duedate','path','points','status') VALUES (:name,:id,:date,:path,:points,:status)")
-    insert = insert.bindparams(name=assignment.name, id=lectid, date=assignment.due_date, path=assignment.path, points=assignment.points, status=assignment.status) 
-    session.execute(insert)
+    #TODO: points and path not yet set
+    assignment = orm.Assignment(name=assignment.name,lectid=lectid, duedate=assignment.due_date, path='', points=0, status=assignment.status)
+    session.add(assignment)
     session.commit()
 
 def update_assignment(assignment: Assignment):
     session = DataBaseManager.instance().create_session()
 
-    update = text("UPDATE 'assignment' SET name=:name, duedate=:date, path=:path, points=:points, status=:status WHERE id=:id")
-    update = update.bindparams(name=assignment.name, date=assignment.due_date, path=assignment.path, points=assignment.points, status=assignment.status, id=assignment.id)
-    session.execute(update)
+    assign = session.query(orm.Assignment).filter(orm.Assignment.id==assignment.id).one()
+    assign.name = assignment.name
+    assign.duedate = assignment.due_date
+    # assignment model doesnt have a path
+    #assign.path = assignment.path
+    # also doesnt have points -> not a bad thing just clarification
+    #assign.points = assignment.points
+    assign.status = assignment.status
     session.commit()
 
 def delete_assignment(assignid: Assignment):
     session = DataBaseManager.instance().create_session()
 
-    delete = text("DELETE FROM 'assignment' WHERE id=:id")
-    delete = delete.bindparams(id=assignid)
-    session.execute(delete)
+    assign = session.query(orm.Assignment).filter(orm.Assignment.id==assignid).one()
+    session.delete(assign)
     session.commit()
+    return assign
 
 
 def get_exercises(assignment: Assignment) -> List[Exercise]:
     session = DataBaseManager.instance().create_session()
 
-    select = text("SELECT * FROM 'file' where assignid = :aid AND exercise = true;")
-    select = select.bindparams(aid=assignment.id)
-    res = session.execute(select)
-    res = [dict(ex) for ex in res]
+    files = session.query(orm.Assignment).filter(orm.Assignment.files.exercise == True).all()
+
+    res = [dict(ex) for ex in files]
     res = [Exercise.from_dict({"id": d["id"], "a_id": d["assignid"], "name": d["name"], "points": d["points"], "path": d["path"]} for d in res)]
     session.commit()
     return res
@@ -69,10 +67,12 @@ def get_exercises(assignment: Assignment) -> List[Exercise]:
 def get_files(assignment: Assignment) -> List[AssignmentFile]:
     session = DataBaseManager.instance().create_session()
 
-    select = text("SELECT * FROM 'file' where assignid = :aid AND exercise = false;")
-    select = select.bindparams(aid=assignment.id)
-    res = session.execute(select)
-    res = [dict(ex) for ex in res]
+    files = session.query(orm.Assignment).filter(orm.Assignment.files.exercise == False).all()
+
+    res = [dict(ex) for ex in files]
     res = [AssignmentFile.from_dict({"id": d["id"], "a_id": d["assignid"], "name": d["name"], "path": d["path"]} for d in res)]
     session.commit()
     return res
+
+
+get_assignments(1)
--- service/grader/service/persistence/submissions.py
+++ service/grader/service/persistence/submissions.py
@@ -5,20 +5,14 @@ from sqlalchemy.orm import Session
 from grader.common.models.user import User
 from grader.common.models.submission import Submission
 from sqlalchemy.sql import text
+from grader.service import orm
 import datetime
 import json 
 
 def get_submissions(user: User, assignid: int,  latest: bool) -> List[Submission]:
     session = DataBaseManager.instance().create_session()
-
-    select = ''
-    if latest:
-        select = text("SELECT * FROM submission WHERE assignid=:id AND username=:name ORDER BY date DESC LIMIT 1") 
-    else:
-        select = text("SELECT * FROM submission WHERE assignid=:id AND username=:name ORDER BY date DESC")
-    select = select.bindparams(id=assignid, name=user.name)
-    res = session.execute(select)
-    res = [dict(x) for x in res]
+    select = session.query(orm.Submission).filter(orm.Submission.username == user.name, orm.Submission.assignid == assignid).all()
+    res = [dict(x) for x in select]
     res = [Submission.from_dict({"id": d["id"], "submitted_at": d["date"], "status": d["status"]} for d in res)]
     session.commit()
     return res
@@ -27,7 +21,6 @@ def get_submissions(user: User, assignid: int,  latest: bool) -> List[Submission
 def submit_assignment(user: User, assignid: int) -> None:
     session = DataBaseManager.instance().create_session()
 
-    insert = text("INSERT INTO 'submission' ('date','assignid','username') VALUES (:date,:id,:name)")
-    insert = insert.bindparams(date=datetime.date.today(), id=assignid, name=user.name)
-    session.execute(insert)
+    submission = orm.Submission(username=user.name,date=datetime.datetime.today(),assignid=assignid)
+    session.add(submission)
     session.commit()
