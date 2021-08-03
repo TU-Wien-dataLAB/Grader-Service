708a25aef2f9e485ea65bee9969ebffa8f4fd240
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-08-03 09:51:16 +0000

    Fixes bug if timestamp is None

--- service/orm/submission.py
+++ service/orm/submission.py
@@ -3,19 +3,31 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum, Bool
 from sqlalchemy.orm import relationship
 from api.models import submission
 
+
 class Submission(Base, Serializable):
-  __tablename__ = "submission"
-  id = Column(Integer, primary_key=True, autoincrement=True)
-  date = Column(DateTime, nullable=False)
-  status = Column(Enum('submitting', 'not_graded', 'automatically_graded', 'manually_graded'), default='submitting', nullable=False)
-  score = Column(Integer, nullable=True)
-  assignid = Column(Integer, ForeignKey('assignment.id'))
-  username = Column(String(255), ForeignKey("user.name"))
-  commit_hash = Column(String(length=40), nullable=False),
+    __tablename__ = "submission"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(DateTime, nullable=False)
+    status = Column(
+        Enum("submitting", "not_graded", "automatically_graded", "manually_graded"),
+        default="submitting",
+        nullable=False,
+    )
+    score = Column(Integer, nullable=True)
+    assignid = Column(Integer, ForeignKey("assignment.id"))
+    username = Column(String(255), ForeignKey("user.name"))
+    commit_hash = (Column(String(length=40), nullable=False),)
 
-  assignment = relationship("Assignment", back_populates="submissions")
-  user = relationship("User", back_populates="submissions")
+    assignment = relationship("Assignment", back_populates="submissions")
+    user = relationship("User", back_populates="submissions")
 
-  @property
-  def model(self) -> submission.Submission:
-      return submission.Submission(id=self.id, submitted_at=(self.date.isoformat("T", "milliseconds") + "Z"), status=self.status, score=self.score)
+    @property
+    def model(self) -> submission.Submission:
+        return submission.Submission(
+            id=self.id,
+            submitted_at=None
+            if self.date is None
+            else (self.date.isoformat("T", "milliseconds") + "Z"),
+            status=self.status,
+            score=self.score,
+        )
