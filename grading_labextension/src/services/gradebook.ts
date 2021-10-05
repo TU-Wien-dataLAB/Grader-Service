af596f968de4a682f945918062c4a3505eca8464
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-10-05 15:47:51 +0000

    Add update to properties in frontend

--- grading_labextension/src/services/gradebook.ts
+++ grading_labextension/src/services/gradebook.ts
@@ -6,35 +6,35 @@ export class GradeBook {
     }
 
     public setComment(notebook: string, cellId: string, comment: string) {
-        this.properties[notebook]["comments_dict"][cellId]["manual_comment"] = comment;
+        this.properties["notebooks"][notebook]["comments_dict"][cellId]["manual_comment"] = comment;
     }
 
     public getComment(notebook: string, cellId: string): string {
-        return this.properties[notebook]["comments_dict"][cellId]["manual_comment"];
+        return this.properties["notebooks"][notebook]["comments_dict"][cellId]["manual_comment"];
     }
 
     public setManualScore(notebook: string, cellId: string, score: number) {
-        this.properties[notebook]["grades_dict"][cellId]["manual_score"] = score;
+        this.properties["notebooks"][notebook]["grades_dict"][cellId]["manual_score"] = score;
     }
 
     public getManualScore(notebook: string, cellId: string): number {
-        return this.properties[notebook]["grades_dict"][cellId]["manual_score"];
+        return this.properties["notebooks"][notebook]["grades_dict"][cellId]["manual_score"];
     }
 
     public setExtraCredit(notebook: string, cellId: string, credit: number) {
-        this.properties[notebook]["grades_dict"][cellId]["extra_credit"] = credit;
+        this.properties["notebooks"][notebook]["grades_dict"][cellId]["extra_credit"] = credit;
     }
 
     public getExtraCredit(notebook: string, cellId: string): number {
-        return this.properties[notebook]["grades_dict"][cellId]["extra_credit"];
+        return this.properties["notebooks"][notebook]["grades_dict"][cellId]["extra_credit"];
     }
 
     public setNeedsManualGrading(notebook: string, cellId: string, needsGrading: boolean) {
-        this.properties[notebook]["grades_dict"][cellId]["needs_manual_grade"] = needsGrading;
+        this.properties["notebooks"]["notebooks"][notebook]["grades_dict"][cellId]["needs_manual_grade"] = needsGrading;
     }
 
     public getNeedsManualGrading(notebook: string, cellId: string): boolean {
-        return this.properties[notebook]["grades_dict"][cellId]["needs_manual_grade"];
+        return this.properties["notebooks"][notebook]["grades_dict"][cellId]["needs_manual_grade"];
     }
 
 }
