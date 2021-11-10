260928963544d4fbc8f7aee4727e1bc20cf1c14c
Matt, Matthias <matthias.matt@tuwien.ac.at>
2021-11-10 16:45:46 +0000

    Adds integration test setup for handlers

---
+++ service/tests/handlers/tornado_test_utils.py
@@ -0,0 +1,69 @@
+import os
+import pytest
+from registry import HandlerPathRegistry
+from server import GraderServer
+from tornado_sqlalchemy import SQLAlchemy
+from alembic.config import Config
+from alembic.command import upgrade
+
+
+@pytest.fixture
+def sql_alchemy_db():
+    cfg = Config(
+        os.path.abspath(os.path.dirname(__file__) + "../../../alembic_test.ini")
+    )
+    db = SQLAlchemy(url="sqlite:///:memory:")
+    engine = db.engine
+    with engine.begin() as connection:
+        cfg.attributes["connection"] = connection
+        # downgrade(cfg, "base")
+        upgrade(cfg, "head")
+
+    yield db
+
+
+@pytest.fixture
+def app(tmpdir, sql_alchemy_db):
+    service_dir = str(tmpdir.mkdir("grader_service"))
+    handlers = HandlerPathRegistry.handler_list()
+    application = GraderServer(
+        grader_service_dir=service_dir,
+        handlers=handlers,
+        db=sql_alchemy_db,
+        cookie_secret="test",
+    )
+    application.hub_api_url = "http:local"
+    yield application
+
+
+@pytest.fixture
+def service_base_url():
+    base_url = "/services/grader"
+    yield base_url
+
+
+@pytest.fixture
+def jupyter_hub_mock_server(httpserver):
+    def for_user(login_user: dict, token: str):
+        httpserver.expect_request(f"/authorizations/token/{token}").respond_with_json(
+            login_user
+        )
+        return httpserver
+
+    yield for_user
+    httpserver.stop()
+
+@pytest.fixture
+def default_user():
+    user = {
+        "kind": "user",
+        "name": "ubuntu",
+        "admin": False,
+        "groups": ["20wle2__instructor", "21wle1__student", "22wle1__instructor"],
+    }
+    yield user
+
+@pytest.fixture
+def default_token():
+    token = "some_token"
+    yield token
