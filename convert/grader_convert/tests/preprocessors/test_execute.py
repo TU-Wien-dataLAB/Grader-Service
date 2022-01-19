71b5a0d64b15d6a8f32f2779efe0428a86402749
Matt, Matthias <matthias.matt@tuwien.ac.at>
2022-01-19 17:23:34 +0100

    Move nbconvert requirements from service to convert Add execute test cases to convert

---
+++ convert/grader_convert/tests/preprocessors/test_execute.py
@@ -0,0 +1,24 @@
+import os
+from .base import BaseTestPreprocessor
+from ...preprocessors import Execute
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.exporters.exporter import ResourcesDict
+
+
+class TestExecute(BaseTestPreprocessor):
+    def test_simple_execute_nbconvert(self):
+        nb = self._read_nb(os.path.join("files", "simple.ipynb"))
+        pp = ExecutePreprocessor(timeout=6000, kernel_name='python3')
+
+        nb, resources = pp.preprocess(nb, {})
+        assert nb is not None
+        assert resources is not None
+
+    def test_simple_execute_convert(self):
+        nb = self._read_nb(os.path.join("files", "simple.ipynb"))
+        pp = Execute(timeout=6000, kernel_name='python3')
+
+        res = ResourcesDict()
+        nb, resources = pp.preprocess(nb, res)
+        assert nb is not None
+        assert resources is not None
