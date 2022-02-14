feb3076cc6a73192fed541d221e757eba34e4457
Matt, Matthias <matthias.matt@tuwien.ac.at>
2022-02-14 19:27:34 +0100

    Add requirements to setup.py in service and convert

--- grader_service/setup.py
+++ grader_service/setup.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 import os
@@ -6,6 +8,11 @@ from grader_service.alembic.migrate import main as migrate_main
 
 _version = '0.1.0'
 
+source_root = Path(".")
+# Read the requirements
+with (source_root / "requirements.txt").open(encoding="utf8") as f:
+    requirements = f.readlines()
+
 setup(
     name='grader-service',
     version=_version,
@@ -13,7 +20,7 @@ setup(
     package_data={'grader_service.alembic': ['alembic.ini']},
     url='https://gitlab.tuwien.ac.at/hpc/datalab/jupyter/grader/grader.git',
     license='MIT',
-    author='Florian Jäger & Matthias Matt',
+    author='Elias Wimmer, Florian Jäger & Matthias Matt',
     author_email='',
     description='Grader service',
     entry_points={
@@ -22,4 +29,6 @@ setup(
             f'grader-service-migrate = {migrate_main.__module__}:main'
         ],
     },
+    python_requires=">=3.8",
+    install_requires=requirements,
 )
