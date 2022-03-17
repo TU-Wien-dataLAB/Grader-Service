1decefd2ee9e01951337d602dbfb58433f6d8f1c
mb-wali <mb_wali@hotmail.com>
2022-03-17 14:55:37 +0100

    my changes

---
+++ grader_service_config.py
@@ -0,0 +1,14 @@
+import os
+
+c.GraderService.service_host = "grader_service"
+# existing directory to use as the base directory for the grader service
+service_dir = os.path.expanduser("/var/lib/grader-service")
+c.GraderService.grader_service_dir = service_dir
+
+c.GraderServer.hub_service_name = "grader"
+c.GraderServer.hub_api_token = "7e272a9df62444de9d2d111b5ff6e70f"
+c.GraderServer.hub_api_url = "http://jupyterlab:8000/hub/api"
+c.GraderServer.hub_base_url = "http://jupyterlab:8000"
+
+c.LocalAutogradeExecutor.base_input_path = os.path.expanduser(os.path.join(service_dir, "convert_in"))
+c.LocalAutogradeExecutor.base_output_path = os.path.expanduser(os.path.join(service_dir, "convert_out"))
--- jupyterhub_labextension_config.py
+++ jupyterhub_labextension_config.py
@@ -1,14 +1,14 @@
 c.JupyterHub.services.append(
     {
         'name': 'grader',
-        'url': 'http://127.0.0.1:4010',
-        'api_token': 'myapitoken'
+        'url': 'http://grader_service:4010',
+        'api_token': '7e272a9df62444de9d2d111b5ff6e70f'
     }
 )
 
-c.JupyterHub.load_groups = {
-    "lect1__instructor": ["jovyan"],
-    "lect1__tutor": ["user2"],
-    "lect1__student": ["user3", "user4"]
-}
+# c.JupyterHub.load_groups = {
+#     "lect1__instructor": ["jovyan"],
+#     "lect1__tutor": ["user2"],
+#     "lect1__student": ["user3", "user4"]
+# }
 
