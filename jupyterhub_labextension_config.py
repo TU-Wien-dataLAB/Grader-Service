cd78a531de9418dfc9329dce6471a1bdeff61d54
Matthias Dusch <matthias.dusch@uibk.ac.at>
2022-03-21 15:34:07 +0100

    lab changes

--- jupyterhub_labextension_config.py
+++ jupyterhub_labextension_config.py
@@ -6,9 +6,9 @@ c.JupyterHub.services.append(
     }
 )
 
-# c.JupyterHub.load_groups = {
-#     "lect1__instructor": ["jovyan"],
-#     "lect1__tutor": ["user2"],
-#     "lect1__student": ["user3", "user4"]
-# }
-
+c.JupyterHub.load_groups ={
+    "lect1__instructor": ["jovyan"],
+    "lect1__tutor": ["user1"],
+    "lect1__student": ["user2", "user3"]
+    }
+    
