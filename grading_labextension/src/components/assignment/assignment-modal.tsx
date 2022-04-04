3960ea06914c3f075362dd23fa7f6e248dc1763c
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2022-04-04 13:53:13 +0200

    Moved functions of assignment modal to control component

--- grading_labextension/src/components/assignment/assignment-modal.tsx
+++ grading_labextension/src/components/assignment/assignment-modal.tsx
@@ -18,7 +18,9 @@ import {AssignmentStatus} from "./assignment-status";
 import {getFiles} from "../../services/file.service";
 import { AgreeDialog } from '../coursemanage/dialog';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import GradingIcon from '@mui/icons-material/Grading';
 import { RepoType } from '../util/repo-type';
+import { AssignmentControlComponent } from './assignment-control';
 
 export interface IAssignmentModalProps {
   lecture: Lecture;
@@ -30,93 +32,24 @@ export interface IAssignmentModalProps {
 
 export const AssignmentModalComponent = (props: IAssignmentModalProps) => {
   const [submissions, setSubmissions] = React.useState(props.submissions);
-  const [path, setPath] = React.useState(`${props.lecture.code}/${props.assignment.name}`);
   const [showFeedback, setShowFeedback] = React.useState(false);
   const [feedbackSubmission, setFeedbackSubmission] = React.useState(null);
-  const [hasFiles, setHasFiles] = React.useState(false);
-  const [dialog, setDialog] = React.useState(false);
-
-  React.useEffect(() => {
-    getFiles(path).then(files => setHasFiles(files.length > 0));
-  }, [props]);
-
-  const fetchAssignmentHandler = async (repo: "user" | "release") => {
-    try {
-      await pullAssignment(props.lecture.id, props.assignment.id, repo);
-      props.showAlert('success', 'Successfully Pulled Repo');
-    } catch (e) {
-      props.showAlert('error', 'Error Fetching Assignment');
-    }
-  }
-
-  const resetAssignmentHandler = async () => {
-    //await pushAssignment(props.lecture.id, props.assignment.id, RepoType.ASSIGNMENT, "Commit before reset");
-    try {
-      await resetAssignment(props.lecture,props.assignment).then( () => fetchAssignmentHandler("user"));
-      props.showAlert('success', 'Successfully Reset Assignment');
-    } catch (e) {
-      props.showAlert('error', 'Error Reseting Assignment');
-    }
-    setDialog(false);
-  }
-
-  const submitAssignmentHandler = async () => {
-    try {
-      await submitAssignment(props.lecture, props.assignment);
-      props.showAlert('success', 'Successfully Submitted Assignment');
-    } catch (e) {
-      props.showAlert('error', 'Error Submitting Assignment');
-    }
-    try {
-      const submissions = await getAllSubmissions(props.lecture, props.assignment, false, false);
-      setSubmissions(submissions)
-    } catch (e) {
-      props.showAlert('error', 'Error Updating Submissions');
-    }
-  }
 
   const openFeedback = (submission: Submission) => {
     setFeedbackSubmission(submission);
     setShowFeedback(true);
   }
 
+  
   return (
     <div style={{overflow: "scroll", height: "100%"}}>
       <ModalTitle title={props.assignment.name}/>
       <Box sx={{mt: 10}}>
         <Typography variant={'h6'} sx={{ml: 2}}>Status</Typography>
         <AssignmentStatus lecture={props.lecture} assignment={props.assignment} submissions={submissions}/>
+        
         <Typography variant={'h6'} sx={{ml: 2}}>Files</Typography>
-        <FilesList path={path} showAlert={props.showAlert} sx={{m: 2, mt: 1}}/>
-        <Stack direction={"row"} spacing={1} sx={{m: 1, ml: 2}}>
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={() => submitAssignmentHandler()}
-          >
-            <PublishRoundedIcon fontSize="small" sx={{mr: 1}}/>
-            Submit
-          </Button>
-          <SplitButton
-            variant="outlined"
-            size="small"
-            icon={<GetAppRoundedIcon fontSize="small" sx={{mr: 1}}/>}
-            options={[
-              {name: "Pull User", onClick: () => fetchAssignmentHandler("user")},
-              {name: "Pull Release", onClick: () => fetchAssignmentHandler("release")}
-            ]}
-            selectedIndex={hasFiles ? 0 : 1}
-          />
-          <Button
-            variant="outlined"
-            size="small"
-            color="error"
-            onClick={() => setDialog(true)}
-          >
-            <RestartAltIcon fontSize="small" sx={{mr: 1}}/>
-            Reset
-          </Button>
-        </Stack>
+        <AssignmentControlComponent lecture={props.lecture} assignment={props.assignment} showAlert={props.showAlert} setSubmissions={setSubmissions}/>
 
         <Typography variant={'h6'} sx={{ml: 2, mt: 3}}>Submissions</Typography>
         <SubmissionList submissions={submissions} openFeedback={openFeedback} sx={{m: 2, mt: 1}}/>
@@ -125,15 +58,7 @@ export const AssignmentModalComponent = (props: IAssignmentModalProps) => {
       <LoadingOverlay onClose={() => setShowFeedback(false)} open={showFeedback} container={props.root}>
         <Feedback lecture={props.lecture} assignment={props.assignment} submission={feedbackSubmission}
                   showAlert={props.showAlert}/>
-      </LoadingOverlay>
-      {/* AgreeDialog for assignment reset */}
-      <AgreeDialog 
-      open={dialog} 
-      title={'Reset Assignment'} 
-      message={'This action will delete your current progress and reset the assignment! \n' +
-      'Therefore you should copy and paste your work to a different directory before progressing. '} 
-      handleAgree={resetAssignmentHandler} 
-      handleDisagree={() => setDialog(false)} />
+      </LoadingOverlay>      
     </div>
     
   );
