c58bb1d2d6c0beb9498c052e674808326a2ada61
Matt, Matthias <matthias.matt@tuwien.ac.at>
2022-05-23 21:31:55 +0200

    Include copyright notice for nbgrader Remove copyright notice for grader convert

--- grader_convert/grader_convert/preprocessors/checkcellmetadata.py
+++ grader_convert/grader_convert/preprocessors/checkcellmetadata.py
@@ -1,8 +1,4 @@
-# Copyright (c) 2022, TU Wien
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
+
 
 import traceback
 from typing import Dict, Tuple
