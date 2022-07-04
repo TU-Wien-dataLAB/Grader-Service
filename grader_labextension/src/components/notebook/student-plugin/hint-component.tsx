ddbce238698962e1306048a0159d14ee9ba81cff
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2022-07-04 09:49:47 +0200

    Added show hint command, widget and component

---
+++ grader_labextension/src/components/notebook/student-plugin/hint-component.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Alert, Box, Collapse } from '@mui/material';
+
+export interface IHintComponentProps {
+  hint: string;
+}
+
+export const HintCompontent = (props: IHintComponentProps) => {
+  const [alert, setAlert] = React.useState(true);
+
+  return (
+    <Collapse in={alert}>
+      <Alert
+        sx={{ mt: 1, mb: 1 }}
+        severity="info"
+        onClose={() => {
+          setAlert(false);
+        }}
+      >
+        {props.hint}
+      </Alert>
+    </Collapse>
+  );
+};
