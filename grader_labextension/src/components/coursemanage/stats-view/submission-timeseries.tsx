2888997ac60ab9bb878e8795e3627c6fd2ad3528
Matt, Matthias <matthias.matt@tuwien.ac.at>
2022-07-05 16:27:58 +0200

    First implementation of submission chart

--- grader_labextension/src/components/coursemanage/stats-view/submission-timeseries.tsx
+++ grader_labextension/src/components/coursemanage/stats-view/submission-timeseries.tsx
@@ -1,90 +1,91 @@
 import * as React from "react";
-import {Lecture} from "../../../model/lecture";
-import {Assignment} from "../../../model/assignment";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  TooltipProps
+} from 'recharts';
+import {Card, CardContent, CardHeader, Typography} from "@mui/material";
+import {IStatsProps} from "./stats";
+import moment from "moment";
 import {Submission} from "../../../model/submission";
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
-import {Card} from "@mui/material";
+import {NameType, ValueType} from "recharts/types/component/DefaultTooltipContent";
 
-export interface ISubmissionTimeSeriesProps {
-  lecture: Lecture;
-  assignment: Assignment;
-  allSubmissions: Submission[];
-  latestSubmissions: Submission[];
-  root: HTMLElement;
-  showAlert: (severity: string, msg: string) => void;
+const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${payload[0].value} Submission${payload[0].value === 1 ? '' : 's'}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const getData = (submissions: Submission[]): { time: number, n: number }[] => {
+  const map = submissions
+    .map(s => moment(s.submitted_at).startOf("day").valueOf())
+    .reduce((acc, v) => {
+      const c = acc.has(v) ? acc.get(v) : 0;
+      acc.set(v, c + 1);
+      return acc;
+    }, new Map<number, number>())
+
+  const dates: Array<number> = [];
+  let currDate = moment(Math.min(...map.keys())).subtract(1, 'days');
+  let lastDate = moment(Math.max(...map.keys()));
+
+  while (currDate.add(1, 'days').diff(lastDate) < 0) {
+    dates.push(currDate.valueOf());
+  }
+
+  return dates.map(d => {
+    if (map.has(d)) {
+      return {time: d, n: map.get(d)}
+    } else {
+      return {time: d, n: 0}
+    }
+  });
 }
 
-export const SubmissionTimeSeries = (props: ISubmissionTimeSeriesProps) => {
-  const testData = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+export const SubmissionTimeSeries = (props: IStatsProps) => {
+  const [data, setData] = React.useState([] as { time: number, n: number }[]);
 
-  const [data, setData] = React.useState(testData);
+  React.useEffect(() => {
+    const d = getData(props.allSubmissions);
+    console.log(d);
+    setData(d);
+  }, [])
 
   return (
-    <Card sx={{height: 300, width: 300}}>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3"/>
-          <XAxis dataKey="name"/>
-          <YAxis/>
-          <Tooltip/>
-          <Legend/>
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
-        </LineChart>
-      </ResponsiveContainer>
+    <Card sx={{height: 300, width: "100%"}}>
+      <CardHeader title={"Submissions"}/>
+      <CardContent sx={{height: "70%", width: "100%"}}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            height={150}
+            width={250}
+            data={data}
+            margin={{
+              top: 5,
+              right: 50,
+              left: 10,
+              bottom: 5,
+            }}
+          >
+            <XAxis dataKey="time" tickFormatter={(unixTime) => moment(unixTime).format('DD MMM')}/>
+            <YAxis dataKey="n"/>
+            <Tooltip label={"Number of Submissions"} content={<CustomTooltip />} />
+            <Line type="monotone" dataKey="n" stroke="#8884d8" activeDot={{r: 8}} strokeWidth={4}/>
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
     </Card>
 
   )
