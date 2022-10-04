8435c6709461d7ba77774515a9f8f2f85b75071c
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2022-10-04 14:25:05 +0200

    Testing volumes and volume mounts

--- grader_service/grader_service/autograding/kube/kube_grader.py
+++ grader_service/grader_service/autograding/kube/kube_grader.py
@@ -154,9 +154,9 @@ class KubeAutogradeExecutor(LocalAutogradeExecutor):
         volumes = [self.volume] + self.extra_volumes
 
         # combine volume mounts
-        volume_mount = [{"name": "data", "mountPath": "var/lib/grader-service"}]
+        volume_mount = [{"name": "data", "mountPath": "convert_in/submission_7"}, {"name": "data", "mountPath": "convert_out/submission_7"}]
         volume_mounts = volume_mount + self.extra_volume_mounts
-
+        self.log.info("Volumes: " + str(volumes) + "\n Volume Mounts: " + str(volume_mounts))
         pod = make_pod(
             name=self.submission.commit_hash,
             cmd=shlex.split(command),
