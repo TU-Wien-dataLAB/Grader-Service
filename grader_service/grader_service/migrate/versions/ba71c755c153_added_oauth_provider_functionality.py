de973a3df941ad0091a86707f28d6a5a56b9b269
Jäger, Florian <florian.jaeger@tuwien.ac.at>
2023-08-18 15:08:06 +0200

    Slice and dice part 4: Added alembic revision script for oauth orms

---
+++ grader_service/grader_service/migrate/versions/ba71c755c153_added_oauth_provider_functionality.py
@@ -0,0 +1,65 @@
+"""Added oauth provider functionality
+
+Revision ID: ba71c755c153
+Revises: a1791b1371ed
+Create Date: 2023-08-18 10:30:24.465356
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import Column, Unicode, Integer, DateTime, Text, \
+    PrimaryKeyConstraint, ForeignKeyConstraint
+
+# revision identifiers, used by Alembic.
+revision = 'ba71c755c153'
+down_revision = 'a1791b1371ed'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('api_token',
+                    Column('username', Unicode(255)),
+                    Column('id', Integer, nullable=True),
+                    Column('hashed', Unicode(255)),
+                    Column('prefix', Unicode(16)),
+                    Column('client_id', Unicode(255)),
+                    Column('session_id', Unicode(255)),
+                    Column('created', DateTime),
+                    Column('expires_at', DateTime),
+                    Column('last_activity', DateTime),
+                    Column('note', Unicode(1023)),
+                    Column('scopes', Text),
+                    PrimaryKeyConstraint('id'),
+                    ForeignKeyConstraint(['username'], ['user.name'])
+                    )
+
+    op.create_table('oauth_client',
+                    Column('id', Integer, nullable=True),
+                    Column('identifier', Unicode(255), unique=True),
+                    Column('description', Unicode(1023)),
+                    Column('secret', Unicode(255)),
+                    Column('redirect_uri', Unicode(1023)),
+                    Column('allowed_scopes', Text),
+                    PrimaryKeyConstraint('id')
+                    )
+
+    op.create_table('oauth_code',
+                    Column('id', Integer, nullable=True),
+                    Column('client_id', Unicode(255)),
+                    Column('code', Unicode(36)),
+                    Column('expires_at', Integer),
+                    Column('redirect_uri', Unicode(1023)),
+                    Column('username', Unicode(255)),
+                    Column('scopes', Text),
+                    PrimaryKeyConstraint("id"),
+                    ForeignKeyConstraint(['client_id'],
+                                         ['oauth_client.identifier']),
+                    ForeignKeyConstraint(['username'], ['user.name'])
+                    )
+
+
+def downgrade():
+    op.drop_table('api_token')
+    op.drop_table('oauth_code')
+    op.drop_table('oauth_client')
