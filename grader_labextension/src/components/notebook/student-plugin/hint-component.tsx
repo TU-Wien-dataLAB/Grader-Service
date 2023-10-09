f83997f760c3bddabf1b8463d6c3ea8be62b51f3
Matt, Matthias <matthias.matt@tuwien.ac.at>
2023-10-09 14:49:33 +0200

    Add dark mode to hint component

--- grader_labextension/src/components/notebook/student-plugin/hint-component.tsx
+++ grader_labextension/src/components/notebook/student-plugin/hint-component.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import { Alert, Collapse } from '@mui/material';
+import { Alert, Collapse, createTheme } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/system';
+import { GlobalObjects } from '../../../index';
 
 export interface IHintComponentProps {
   hint: string;
@@ -7,11 +10,18 @@ export interface IHintComponentProps {
 }
 
 export const HintComponent = (props: IHintComponentProps) => {
+  const theme = createTheme({
+    palette: { mode: (GlobalObjects.themeManager.isLight(GlobalObjects.themeManager.theme)) ? 'light' : 'dark' }
+  });
+
   return (
-    <Collapse in={props.show}>
-      <Alert sx={{ mt: 1, mb: 1 }} severity="info">
-        {props.hint}
-      </Alert>
-    </Collapse>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Collapse in={props.show}>
+        <Alert sx={{ mt: 1, mb: 1 }} severity='info'>
+          {props.hint}
+        </Alert>
+      </Collapse>
+    </ThemeProvider>
   );
 };
