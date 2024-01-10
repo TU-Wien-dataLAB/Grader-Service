f31e4f235c01ba3e3187b0daade54e4c86d1f10c
Marijana Petojevic <marijana.petojevic@tuwien.ac.at>
2024-01-10 15:24:44 +0100

    chore: added new db revision for feedback status

---
+++ grader_service/grader_service/migrate/versions/9cfeb0faa0c0_add_feedback_status.py
@@ -0,0 +1,24 @@
+"""Add feedback status
+
+Revision ID: 9cfeb0faa0c0
+Revises: 4620c13728e7
+Create Date: 2024-01-10 15:16:36.273796
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9cfeb0faa0c0'
+down_revision = '4620c13728e7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
