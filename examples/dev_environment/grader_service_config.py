562166c2a0b1852d3254e2e50cb2304d7aaa4570
Matt, Matthias <matthias.matt@tuwien.ac.at>
2024-09-06 08:41:44 +0200

    Update dev-environment grader_service_config.py with post auth hook

--- examples/dev_environment/grader_service_config.py
+++ examples/dev_environment/grader_service_config.py
@@ -1,6 +1,13 @@
 import os
+
+from grader_service.auth.auth import Authenticator
 from grader_service.autograding.kube.kube_grader import KubeAutogradeExecutor
 from grader_service.autograding.local_grader import LocalAutogradeExecutor
+from grader_service.handlers.base_handler import BaseHandler
+from grader_service.orm import User, Lecture
+from grader_service.orm.base import DeleteState
+from grader_service.orm.lecture import LectureState
+from grader_service.orm.takepart import Scope, Role
 
 # from grader_service.autograding.docker_grader import DockerAutogradeExecutor
 
@@ -12,9 +19,6 @@ service_dir = os.path.expanduser("~/grader_service_dir")
 c.GraderService.grader_service_dir = service_dir
 
 c.JupyterHubGroupAuthenticator.hub_api_url = "http://127.0.0.1:8081/hub/api"
-
-# c.LocalAutogradeExecutor.relative_input_path = "convert_in"
-# c.LocalAutogradeExecutor.relative_output_path = "convert_out"
 
 assert issubclass(KubeAutogradeExecutor, LocalAutogradeExecutor)
 # c.RequestHandlerConfig.autograde_executor_class = DockerAutogradeExecutor
@@ -34,6 +38,61 @@ c.CeleryApp.conf = dict(
     task_always_eager=True
 )
 c.CeleryApp.worker_kwargs = dict(concurrency=1, pool="prefork")
+
+
+def get_role_from_auth(auth_state):
+    user_role = 'student'
+    for role in auth_state['https://purl.imsglobal.org/spec/lti/claim/roles']:
+        if role.find('Instructor') >= 1:
+            user_role = 'instructor'
+            break
+    return user_role
+
+
+def post_auth_hook(authenticator: Authenticator, handler: BaseHandler, authentication: dict):
+    print("####### POST AUTH HOOK")
+    session = handler.session
+    log = handler.log
+    auth_state = authentication["auth_state"]
+
+    username = authentication["name"]
+    user_model: User = session.query(User).get(username)
+    if user_model is None:
+        user_model = User()
+        user_model.name = username
+        session.add(user_model)
+        session.commit()
+
+    lecture_code = auth_state["https://purl.imsglobal.org/spec/lti/claim/context"]["label"].replace(" ", "")
+    lecture = session.query(Lecture).filter(Lecture.code == lecture_code).one_or_none()
+    if lecture is None:
+        lecture = Lecture()
+        lecture.code = lecture_code
+        lecture.name = lecture_code
+        lecture.state = LectureState.active
+        lecture.deleted = DeleteState.active
+        session.add(lecture)
+        session.commit()
+
+    lti_role = get_role_from_auth(auth_state)
+    scope = Scope[lti_role.lower()]
+    log.info(f'Determined role {scope.name} for user {username}')
+
+    role = session.query(Role).filter(Role.username == username, Role.lectid == lecture.id).one_or_none()
+    if role is None:
+        log.info(f'No role for user {username} in lecture {lecture_code}... creating role')
+        role = Role(username=username, lectid=lecture.id, role=scope)
+        session.add(role)
+        session.commit()
+    else:
+        log.info(f'Found role {role.role.name} for user {username}  in lecture {lecture_code}... updating role to {scope.name}')
+        role.role = scope
+        session.commit()
+
+    return authentication
+
+
+c.Authenticator.post_auth_hook = post_auth_hook
 
 ## authenticator
 
