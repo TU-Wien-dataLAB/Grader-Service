cab3463364fe6cc6a24a43ca6dc5b79143a8de02
Matt, Matthias <matthias.matt@tuwien.ac.at>
2024-09-09 15:29:05 +0200

    Update alembic migrate with GraderService instance to get db_url instead of regex pattern

--- grader_service/migrate/migrate.py
+++ grader_service/migrate/migrate.py
@@ -5,11 +5,10 @@
 # LICENSE file in the root directory of this source tree.
 
 # Python script that will apply the migrations up to head
-import argparse 
+import argparse
 import alembic.config
 import os
 import re
-
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,6 +16,7 @@ alembic_args = [
     '-c', os.path.join(here, 'alembic.ini'),
     'upgrade', 'head'
 ]
+
 
 def getargs():
     "Give the option of passing args on the commandline"
@@ -27,46 +27,30 @@ def getargs():
     return parser.parse_args()
 
 
-def get_config_file(cfile):
-    with open(cfile, 'r') as f:
-        lines = f.read()
-    return lines
-
-
-def get_matching_config(text, patt: re.Pattern):
-    """Load a config value by matching a line in text that contains pattern 
-    Assume the line contains a 'key = value' type of pair, split the line at 
-    equals and return the value part.
-    """
-    lines = text.split('\n')
-    result = None
-    for line in lines: 
-        match = re.match(patt, line)
-        if match:
-           _, db_url, _ = match.groups()
-           result = db_url
-    return result 
-
-
-DB_URL_PATT = re.compile(r"(c.GraderService.db_url = ')(.*?)(')")
-
-def load_custom_config(cfile):
+def load_custom_config(config):
     """Load a custom config file that uses the same format as Juypterhub and
     GraderService"""
-    global DB_URL_PATT
-    db_url = get_matching_config(cfile, DB_URL_PATT)
+    from grader_service.main import GraderService
+    service = GraderService.instance()
+    service.load_config_file(config)
+    service.set_config()
+    db_url = service.db_url
     if db_url is None:  # no match, do nothing
-        return 
+        return
     environment = {}
     environment.update({
         "GRADER_DB_URL": db_url,
-        })
+    })
     # use environment to set global env vars
     os.environ.update(environment)
+
 
 def main():
     args = getargs()
     if args.config:
-        cfile = get_config_file(args.config)
-        load_custom_config(cfile)
+        load_custom_config(args.config)
     alembic.config.main(argv=alembic_args)
+
+
+if __name__ == '__main__':
+    main()
